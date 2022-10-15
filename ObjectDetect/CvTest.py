fff8573f22f3a7264a33ee23115ec952613759bf
samarthak <samarthakrao@gmail.com>
2022-10-15 12:10:28 +0530

    cv

---
+++ ObjectDetect/CvTest.py
@@ -0,0 +1,14 @@
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2' 
+import cv2
+
+path='./ObjectDetect/dataset/'
+file=os.listdir(path)
+for f in file:
+    img=cv2.imread(os.path.join(path,f))
+    img=cv2.resize(img,dsize=(227,227))
+    cv2.imshow('img',img)
+    cv2.waitKey(0)
+    #img=img.reshape(-1,227,227,3)
+    w,h=img.shape[:2]
+    print(w,h)
